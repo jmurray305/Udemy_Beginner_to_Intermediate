0d2605bdea703508779f46903b128cbfd8ffbf67
Justin Murray <j.murray305@gmail.com>
2019-08-05 14:45:27 -0600

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,5 +1,13 @@
+#Caesar Cipher input plaintext and number of shifts for encrypted message
 def caesar(plaintext, shift):
     alphabet = string.ascii_lowercase
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     table = str.maketrans(alphabet, shifted_alphabet)
     return plaintext.translate(table)
+
+#Return True if the string "cat" and "dog" appear the same number of times in the given string.
+def sameAmount(S, word1, word2):
+    if S.count(word1) == S.count(word2):
+        return True
+    else:
+        return False
